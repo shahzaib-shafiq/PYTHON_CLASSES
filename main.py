2e9a8a659d4285d9c710bb1815cf80ecc5a8ab72
SHAHZAIB SHAFIQ <73356944+shahzaibshafiq100@users.noreply.github.com>
2023-02-14 23:06:10 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+fortunes = ["Good things come to those who wait.",
+            "Patience is a virtue.",
+            "The early bird gets the worm.",
+            "A wise man once said, everything in its own time and place.",
+            "Fortune cookies rarely share fortunes."
+            "If the world rests on hope then why don’t you expect yourself.”"
+            "Never give up on your hard work, one day you will succeed."
+            "Education is the key to unlocking the golden door of freedom.” – George Washington Carver "
+            ]
+
+print(random.choice(fortunes))
+
+
+
